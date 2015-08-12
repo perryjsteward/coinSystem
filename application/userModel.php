78a93e43a3a5f3899b722cd41f4190cd2c1b82f3
thobed <212411640@LI-4ZHVM32.amer.consind.ge.com>
2015-08-12 10:27:34 -0400

    CreateUser

--- application/userModel.php
+++ application/userModel.php
@@ -9,7 +9,7 @@
 			$this->db = new Database();
 			$this->db->connect();
 		}
-
+		// pull the entire list of users
 		public function getUserList() {
 			$result = $this->db->query('select * FROM personal');
 			$users = mysqli_fetch_all($result['result']);
@@ -21,7 +21,7 @@
 			}
 			return $userList;
 		}
-		
+		// select a specific user based on there SSO
 		public function getUser($SSO) {
 			//$db = new Database();
 			//$db->connect();
@@ -33,7 +33,7 @@
 				
 			return $userObj;
 		}
-		
+		// update the users country
 		public function setUserCountry($SSO, $Country ) {
 			$result = $this->db->query("UPDATE personal SET Country='" . $Country . "' WHERE SSO = " . $SSO . "");
 			if($result == 1) {
@@ -42,7 +42,7 @@
 				return false;
 			}
 		}
-		
+		// update the users business
 		public function setUserBusiness($SSO, $Business ) {
 			$result = $this->db->query("UPDATE personal SET Business='" . $Business . "' WHERE SSO = " . $SSO . "");
 			if($result == 1) {
@@ -51,7 +51,7 @@
 				return false;
 			}
 		}	
-			
+		// update the users Email address
 		public function setUserEmail($SSO, $Email ) {
 			$result = $this->db->query("UPDATE personal SET Email='" . $Email . "' WHERE SSO = " . $SSO . "");
 			if($result == 1) {
@@ -61,7 +61,7 @@
 			}
 		}	
 		
-		
+		// update the users role or rotation with in the business.
 		public function setUserRoleOrRoatation($SSO, $RoleOrRotation ) {
 			$result = $this->db->query("UPDATE personal SET RoileOrRoatation='" . $RoleOrRotation . "' WHERE SSO# = " . $SSO . "");
 			if($result == 1) {
@@ -71,7 +71,7 @@
 			}
 		}
 
-		
+		//update users permissions to view the application
 		public function setUserPermissions($SSO, $Permissions ) {
 			$result = $this->db->query("UPDATE personal SET Permissions='" . $Permissions . "' WHERE SSO# = " . $SSO . "");
 			if($result == 1) {
@@ -80,7 +80,7 @@
 				return false;
 			}
 		}
-		
+		// update users admin access
 		public function setUserAdmin($SSO, $Admin ) {
 			$result = $this->db->query("UPDATE personal SET Admin='" . $Admin . "' WHERE SSO# = " . $SSO . "");
 			if($result == 1) {
@@ -90,9 +90,22 @@
 			}
 		}
 		
+		// update User Password
 		public function setUserPassword($SSO, $Password ) {
 			$result = $this->db->query("UPDATE personal SET Password='" . $Password . "' WHERE SSO# = " . $SSO . "");
 			if($result == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		// create a new user
+		public function createUSer($SSO, $LName, $FName, $Country, $Business, $Email, $RoleOrRotation, $Permissions, $Admin, $Password) {
+			$result = $this->db->query("INSERT INTO personal (SSO, LName, FName, Country, Business, Email, RoleOrRotation, Permissions, Admin, Password) VALUES('" . $SSO . 
+				"','" . $LName . "','" . $FName . "','". $Country . "','". $Business . "','". $Email . "','". $RoleOrRotation . "','". $Permissions . " ','" . $Admin . "','" . $Password . "')");
+			//echo count()
+			if(count($result['result']) == 1) {
 				return true;
 			} else {
 				return false;
