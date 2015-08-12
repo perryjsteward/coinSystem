1b7a59d8941e224df46f434c817584e4be9c7c69
Perry Steward <perryjsteward@gmail.com>
2015-08-11 20:22:10 -0400

    controller edit
    restructuring

--- application/controllers/storyController.php
+++ application/controllers/storyController.php
@@ -3,16 +3,20 @@ include_once(MODEL_PATH . "/model.php");
 
 class StoryController {
 	public $model;
-	
+	//constructor initiates a new model object
 	public function __constructor(){
 		$this->model = new Model();
 	}
-	
-	public function invoke(){
-		
-		//if statement to handle http request
-		if(!isset($_GET['book'])){
-			$books = $this->model
+	//invoke method for calling model class and obtaining story arrays
+	public function invoke(){	
+		//if statement to handle http request for list or specific story
+		if(!isset($_GET['story'])){
+			$story = $this->model->getStoryList();
+			include('story.php?view=list');
+		} else {
+			//show specific requested story
+			$story = $this->model->getStory($_GET['book']);
+			include ('story.php?view=specific');
 		}
 	}
 }
