1f2a4e7bb0727105520befe1037af4278a99794b
Perry Steward <perryjsteward@gmail.com>
2015-08-13 07:44:47 -0400

    Story views and controller

--- application/models/storyModel.php
+++ application/models/storyModel.php
@@ -9,9 +9,25 @@
 			$this->db = new Database();
 			$this->db->connect();
 		}
+		
+		//get Approves Stories
+		public function getApprovedList() {
+			//$db = new Database();
+			//$db->connect();
+			//global $db;
+			$result = $this->db->query('select * FROM story WHERE status = "approved"');
+			$stories = mysqli_fetch_all($result['result']);
+			
+			$storyList = array();
+			foreach($stories as $story) {
+				$storyObj = new Story($story[0], $story[1], $story[2], $story[3], $story[4], $story[5], $story[6], $story[7], $story[8], $story[9]); 
+				array_push($storyList, $storyObj);
+			}
+			return $storyList;
+		}
 
+		
 		// returns an array of story objects
-	
 		public function getStoryList() {
 			//$db = new Database();
 			//$db->connect();
--- application/views/story-create.php
+++ application/views/story-create.php
@@ -1,17 +1,23 @@
 
-<div class="container" style="width: 600px; margin-top: 50px;">
-<div class="alert alert-warning" role="alert">You've missed something on the form</div>
-<div class="alert alert-danger" role="alert">You do not have the ability to give a coin</div>
+<div class="container" style="width: 600px; margin-top: 0px;">
 
+<?php
+	//if error
+	$warning = '<div class="alert alert-warning" role="alert">Youve missed something on the form</div>';
+	$danger = '<div class="alert alert-danger" role="alert">You do not have the ability to give a coin</div>'
+?>
+<div class="page-header">
+	<h1>The ITLP Stories</h1>
+</div>
 <form>
   <div class="form-group">
     <label for="receiverSso">Receiver SSO</label>
-    <input type="text" class="form-control" id="ReceiverSso" placeholder="Receiver SSO">
+    <input type="text" class="form-control" id="ReceiverSso" placeholder="Receiver SSO" required>
   </div>
   <div class="form-group">
     <label for="value1">First Value</label>
     <select class="form-control">
-		<option selected="selected" disabled="disabled">Select Value</option>
+		<option selected="selected" disabled="disabled" required>Select Value</option>
 		<option>Integrity</option>
 		<option>Humility</option>
 		<option>Educator</option>
@@ -23,7 +29,7 @@
   <div class="form-group">
     <label for="value2">Second Value</label>
     <select class="form-control">
-		<option selected="selected" disabled="disabled">Select Value</option>
+		<option selected="selected" disabled="disabled" required>Select Value</option>
 		<option>Integrity</option>
 		<option>Humility</option>
 		<option>Educator</option>
@@ -35,7 +41,7 @@
   <div class="form-group">
     <label for="value1">Third Value</label>
     <select class="form-control">
-		<option selected="selected" disabled="disabled">Select Value</option>
+		<option selected="selected" disabled="disabled" required>Select Value</option>
 		<option>Integrity</option>
 		<option>Humility</option>
 		<option>Educator</option>
@@ -45,9 +51,13 @@
 	</select>
   </div>
   <div class="form-group">
-	<label for="story">Story</label>
-	<textarea class="form-control" id="story" rows="5"></textarea>
+	<label for="story">Title</label>
+	<input type="input" class="form-control" id="title" placeholder="Enter Title" required>
 	</div>
+  <div class="form-group">
+	<label for="story">Story</label>
+	<textarea class="form-control" id="story" rows="5" required></textarea>
+  </div>
 	
 	<!-- Hidden inputs for database inputs, PHP used to enter values-->
 	<input type="hidden" class="form-control" id="giverSSo" value="">
